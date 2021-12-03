a18831d61af4d342e82c3cf3563d0ba3121b52f8
Amin1975 <amin.asvadi@gmail.com>
2021-12-03 14:38:42 +0330

    add all dependenceis

---
+++ app/src/main/java/com/devillas/daggerhiltandretrofit/utile/Constance.kt
@@ -0,0 +1,7 @@
+package com.devillas.daggerhiltandretrofit.utile
+
+object Constance {
+    const val CLIENT_ID :String ="JdiWG6_JZMkK6QZTg-Bz4sePVH77vL9_l1_3495cNWM"
+    const val END_POINT :String ="/photos"
+    const val BASE_URL :String ="https://api.unsplash.com"
+}
